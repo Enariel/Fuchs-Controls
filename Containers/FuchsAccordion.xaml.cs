6e11e8daf8613db0cdf148816f26cbe427bb7b27
Oliver Conover <fuchsfarbeart@outlook.com>
2026-01-11 12:47:03 -0500

    Implement expandable accordion component and integrate into ItemsPage.
    Added the FuchsAccordion control with customizable properties (e.g., Title, IsExpanded, HeaderBackgroundColor) and animated expand/collapse functionality. Replaced the unsorted items section in ItemsPage with the new FuchsAccordion for better UI organization.

--- Containers/FuchsAccordion.xaml.cs
+++ Containers/FuchsAccordion.xaml.cs
@@ -14,8 +14,78 @@ namespace FuchsControls.Containers;
 
 public partial class FuchsAccordion : ContentView
 {
-	public FuchsAccordion()
-	{
-		InitializeComponent();
-	}
+    public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+        nameof(Title), typeof(string), typeof(FuchsAccordion), string.Empty);
+
+    public static readonly BindableProperty IsExpandedProperty = BindableProperty.Create(
+        nameof(IsExpanded), typeof(bool), typeof(FuchsAccordion), false, propertyChanged: OnIsExpandedChanged);
+
+    public static readonly BindableProperty AccordionContentProperty = BindableProperty.Create(
+        nameof(AccordionContent), typeof(View), typeof(FuchsAccordion));
+
+    public static readonly BindableProperty HeaderBackgroundColorProperty = BindableProperty.Create(
+        nameof(HeaderBackgroundColor), typeof(Color), typeof(FuchsAccordion), Colors.Transparent);
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public bool IsExpanded
+    {
+        get => (bool)GetValue(IsExpandedProperty);
+        set => SetValue(IsExpandedProperty, value);
+    }
+
+    public View AccordionContent
+    {
+        get => (View)GetValue(AccordionContentProperty);
+        set => SetValue(AccordionContentProperty, value);
+    }
+
+    public Color HeaderBackgroundColor
+    {
+        get => (Color)GetValue(HeaderBackgroundColorProperty);
+        set => SetValue(HeaderBackgroundColorProperty, value);
+    }
+
+    public FuchsAccordion()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnHeaderTapped(object sender, EventArgs e)
+    {
+        IsExpanded = !IsExpanded;
+    }
+
+    private static async void OnIsExpandedChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is FuchsAccordion accordion)
+        {
+            await accordion.AnimateExpansion((bool)newValue);
+        }
+    }
+
+    private async Task AnimateExpansion(bool expanded)
+    {
+        if (expanded)
+        {
+            ContentContainer.Opacity = 0;
+            ContentContainer.TranslationY = -10;
+            await Task.WhenAll(
+                ContentContainer.FadeToAsync(1, 250, Easing.CubicOut),
+                ContentContainer.TranslateToAsync(0, 0, 250, Easing.CubicOut),
+                ChevronLabel.RotateToAsync(180, 250, Easing.CubicInOut)
+            );
+        }
+        else
+        {
+            await Task.WhenAll(
+                ContentContainer.FadeToAsync(0, 200, Easing.CubicIn),
+                ChevronLabel.RotateToAsync(0, 250, Easing.CubicInOut)
+            );
+        }
+    }
 }
