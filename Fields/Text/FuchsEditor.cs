b790b477c45c2ad54669e56810f7eb41f7681f8a
Oliver Conover <fuchsfarbeart@outlook.com>
2026-01-10 19:12:07 -0500

    Add new text field controls and style resources
    - Added `FuchsEditor` control with configurable properties and orientation support.
    - Enhanced `FuchsEntry` control to include layout and bindings for entry fields.
    - Introduced global color and style resources (`StyleColors.xaml`, `ControlStyles.xaml`).
    - Updated project file to include new resource files and directories.

---
+++ Fields/Text/FuchsEditor.cs
@@ -0,0 +1,56 @@
+﻿#region Meta
+// FuchsControls
+// Created: 10/01/2026
+// Modified: 10/01/2026
+#endregion
+
+namespace FuchsControls.Fields;
+
+public class FuchsEditor : TextEditorBase
+{
+	public FuchsEditor()
+	{
+		var stack = new StackLayout
+		{
+			Orientation = GetOrientation(),
+			Spacing = 5
+		};
+
+		var label = new Label();
+		label.SetBinding(Microsoft.Maui.Controls.Label.TextProperty, new Binding(nameof(Label), source: this, mode: BindingMode.OneWay));
+
+		var editor = new Editor();
+		editor.SetBinding(InputView.KeyboardProperty, new Binding(nameof(Keyboard), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Editor.PlaceholderProperty, new Binding(nameof(Placeholder), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(InputView.MaxLengthProperty, new Binding(nameof(MaxLength), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Editor.AutoSizeProperty, new Binding(nameof(AutoSize), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Editor.HeightRequestProperty, new Binding(nameof(EditorHeight), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Editor.TextProperty, new Binding(nameof(Text), source: this, mode: BindingMode.TwoWay));
+		
+
+		stack.Children.Add(label);
+		
+#if WINDOWS
+        if (!string.IsNullOrEmpty(HelpText))
+            ToolTipProperties.SetText(label, new Binding(nameof(HelpText), source: this));
+#else
+		var helpText = new Label();
+		helpText.SetBinding(Microsoft.Maui.Controls.Label.TextProperty, new Binding(nameof(HelpText), source: this));
+		stack.Children.Add(helpText);
+#endif
+		Content = stack;
+	}
+	
+	private StackOrientation GetOrientation()
+	{
+		switch (Orientation)
+		{
+			case FieldOrientation.Horizontal:
+				return StackOrientation.Horizontal;
+			case FieldOrientation.Vertical:
+				return StackOrientation.Vertical;
+			default:
+				return StackOrientation.Vertical;
+		}
+	}
+}
--- Fields/Text/FuchsEntry.cs
+++ Fields/Text/FuchsEntry.cs
@@ -1,12 +1,45 @@
 ﻿#region Meta
+
 // FuchsControls
 // Created: 10/01/2026
 // Modified: 10/01/2026
+
 #endregion
 
 namespace FuchsControls.Fields;
 
 public class FuchsEntry : TextFieldBase, ITextField
 {
-	
+	public FuchsEntry()
+	{
+		var stack = new StackLayout();
+		stack.Orientation = GetOrientation();
+
+		var label = new Label();
+		label.SetBinding(Microsoft.Maui.Controls.Label.TextProperty, new Binding(nameof(Label), source: this, mode: BindingMode.OneWay));
+		var editor = new Entry();
+		editor.SetBinding(Entry.KeyboardProperty, new Binding(nameof(Keyboard), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Entry.ReturnTypeProperty, new Binding(nameof(ReturnType), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(PlaceholderProperty, new Binding(nameof(Placeholder), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(MaxLengthProperty, new Binding(nameof(MaxLength), source: this, mode: BindingMode.OneWay));
+		editor.SetBinding(Entry.TextProperty, new Binding(nameof(Text), source: this, mode: BindingMode.TwoWay));
+		editor.SetBinding(InputView.IsReadOnlyProperty, new Binding(nameof(IsReadOnly), source: this, mode: BindingMode.TwoWay));
+
+		stack.Children.Add(label);
+		stack.Children.Add(editor);
+		Content = stack;
+	}
+
+	private StackOrientation GetOrientation()
+	{
+		switch (Orientation)
+		{
+			case FieldOrientation.Horizontal:
+				return StackOrientation.Horizontal;
+			case FieldOrientation.Vertical:
+				return StackOrientation.Vertical;
+			default:
+				return StackOrientation.Vertical;
+		}
+	}
 }
