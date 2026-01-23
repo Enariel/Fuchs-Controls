7927a508e5990c6cfab2b49ac19c30f14c1052ee
Oliver Conover <fuchsfarbeart@outlook.com>
2026-01-23 05:58:50 -0500

    Add `StringIsNotNullOrEmptyConverter`
    - Added a new `StringIsNotNullOrEmptyConverter` implementing `IValueConverter` to check for non-null or non-empty strings.
    - Includes `Convert` and `ConvertBack` methods for seamless use in data bindings.

---
+++ Converters/StringIsNotNullOrEmptyConverter.cs
@@ -0,0 +1,24 @@
+#region Meta
+// FuchsControls
+// Created: 23/01/2026
+// Modified: 23/01/2026
+#endregion
+
+using System.Globalization;
+
+namespace FuchsControls;
+
+public class StringIsNotNullOrEmptyConverter : IValueConverter
+{
+	/// <inheritdoc />
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return value is string stringValue && !string.IsNullOrEmpty(stringValue);
+	}
+
+	/// <inheritdoc />
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return null;
+	}
+}
